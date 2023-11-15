ef4ec3dc0bf52610dccd83ecae44875d55235273
SivaK-98 <hastthakanni@outlook.com>
2023-11-15 20:03:41 +0530

    Adding main and python fles

---
+++ main.py
@@ -0,0 +1,93 @@
+from flask import Flask, flash, redirect, render_template, request, url_for
+
+import mongodb
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['MESSAGE_FLASHING_OPTIONS'] = {'duration': 5}
+
+
+@app.route('/')
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+  return render_template("home.html")
+
+
+@app.route("/content", methods=['POST', 'GET'])
+def content():
+  if request.method == "POST":
+
+    button = request.form.get("content_button")
+    if button == "login":
+      #print("logged in")
+      global email
+      global password
+      global data
+      global account_id
+      email = request.form.get("email")
+      #print("email:", email)
+      password = request.form.get("password")
+      #print("password:", password)
+      result = mongodb.login(email, password)
+      if result == "invalid_email":
+        flash("Invalid email ID!!", "error")
+        return redirect(url_for('home'))
+      elif result == "invalid_password":
+        flash("Invalid Password !!", "error")
+        return redirect(url_for('home'))
+      else:
+        #print("result got from mongo db:", result)
+        data = result[0]
+        account_id = result[1]
+        #print("Local account_id:", account_id)
+        #print("data", data)
+        #print("account_id: ", account_id)
+        return render_template("content.html", item_name=data)
+    elif button == "remove":
+      remove_app = request.form.get("removeapp")
+      remove_user = request.form.get("removeuser")
+      response = mongodb.remove_entry(remove_app, remove_user, account_id)
+      print(response)
+      result = mongodb.login(email, password)
+      data = result[0]
+      print(data)
+      return render_template("content.html", item_name=data)
+    else:
+      print("you used add me")
+      print("account id from add", account_id)
+      add_app = request.form.get("add_app")
+      add_user = request.form.get("add_user")
+      add_pword = request.form.get("add_pass")
+      print(add_app)
+      print("Global account_id:", account_id)
+      response = mongodb.add_entry(add_app, add_user, add_pword, account_id)
+      result = mongodb.login(email, password)
+      data = result[0]
+      print(data)
+
+      return render_template("content.html", item_name=data)
+
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+  return render_template("signup.html")
+
+
+@app.route("/create_account", methods=['POST', 'GET'])
+def create_account():
+  email = request.form.get("email")
+  user = request.form.get("user")
+  password = request.form.get("password")
+  mobile = request.form.get("mobile")
+  result = mongodb.create_account(email, user, mobile, password)
+  result = str(result)
+
+  if result == "True":
+    return render_template("home.html")
+  elif result == "duplicate":
+    print("signup result:", result)
+    flash("ID Already Used!!", "error")
+    return redirect(url_for('signup'))
+
+
+app.run(host='0.0.0.0', port=81)
---
+++ mongodb.py
@@ -0,0 +1,125 @@
+import crypt
+import datetime
+
+import pymongo
+from pymongo.errors import DuplicateKeyError
+
+client = pymongo.MongoClient(
+    "mongodb+srv://awstestuser1998:awstestuser1998@cluster0.nb2lq1w.mongodb.net/"
+)
+db = client["auth"]
+auth_db = db["users"]
+db.users.create_index([('email', pymongo.ASCENDING)], unique=True)
+
+
+def login(email, password):
+  try:
+    result = auth_db.find_one({"email": email}, {"_id": 0})
+    #print("Result from mongo DB", result)
+    if result != None:
+      key = result['key']
+      got_password = result["password"]
+      data_pass = (got_password, key)
+      decrypted = crypt.decrypt(data_pass)
+      flag = False
+      if decrypted == password:
+        flag = True
+        flag = str(flag)
+        account_id = result["account_id"]
+        user_db = db[str(account_id)]
+        secret_list = []
+        record_list = []
+
+        #print("User DB:", user_db)
+        for record in user_db.find():
+          #print("Record after first for loop", record)
+          record_list.append(record)
+        count = 0
+        for items in record_list:
+          #print("items:", items)
+          if "app" in items:
+            count += 1
+            #print("appname from the item:", items["app"])
+            app = items["app"]
+            user = items["user"]
+            enc_pass = items["password"]
+            enc_key = items["key"]
+            data = (enc_pass, enc_key)
+            dec_pass = crypt.decrypt(data)
+            #print("dec_pass_key:", dec_pass)
+            secret_dict = {}
+            secret_dict["app"] = app
+            secret_dict["user"] = user
+            secret_dict["password"] = dec_pass
+            secret_dict["count"] = count
+            #print("dictionary:", secret_dict)
+            secret_list.append(secret_dict)
+        #print("Secret List from mongo db:", secret_list)
+        return secret_list, account_id
+        #print("Total Record:", record_list)
+      else:
+        return "invalid_password"
+    else:
+      return "invalid_email"
+  except Exception as e:
+    print(e)
+    return e
+
+
+def create_account(email, user, mobile, password):
+  try:
+    import random
+    r = random.randint(1111, 9999)
+    user_db = db[str(r)]
+    created_time = datetime.datetime.now()
+    encrypt_data = crypt.encryptor(password)
+    encrypte_pass = encrypt_data[0]
+    key = encrypt_data[1]
+    query = {
+        "email": email,
+        "user": user,
+        "mobile": mobile,
+        "password": encrypte_pass,
+        "key": key,
+        "account_id": r,
+        "created_time": created_time
+    }
+    auth_db.insert_one(query)
+    query2 = {"created time": created_time}
+    user_db.insert_one(query2)
+    return True
+  except DuplicateKeyError:
+    print("Duplicate ID not allowed")
+    return "duplicate"
+
+
+def remove_entry(remove_app, remove_user, account_id):
+  try:
+    user_db = db[str(account_id)]
+    query = {"app": remove_app, "user": remove_user}
+    user_db.delete_one(query)
+    return "removed"
+  except Exception as e:
+    print(e)
+    return False
+
+
+def add_entry(add_app, add_user, add_pword, account_id):
+  try:
+    user_db = db[str(account_id)]
+    print(user_db)
+    data = crypt.encryptor(add_pword)
+    print("data from add entry:", data)
+    encrypte_pass = data[0]
+    key = data[1]
+    query = {
+        "app": add_app,
+        "user": add_user,
+        "password": encrypte_pass,
+        "key": key
+    }
+    user_db.insert_one(query)
+    return "added"
+  except Exception as e:
+    print(e)
+    return e
