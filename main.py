335b1a501e05af531e2372c16e9294424ed27a7f
SivaK-98 <hastthakanni@outlook.com>
2023-11-18 13:16:00 +0000

    Verified OTP successfully

--- main.py
+++ main.py
@@ -108,22 +108,41 @@ current_session = {}
 
 @app.route("/verify_otp", methods=["POST", "GET"])
 def verify_otp():
-  email = request.form.get("email")
-  admin = request.form.get("adminemail")
-  user = request.form.get("user")
-  import generate_otp
-  result = generate_otp.generate_opt(email)
-  current_session["OTP"] = result
-  return render_template("verify_otp.html", otp=result)
+  data = {}
+  if request.method == "POST":
+    button = request.form.get("send_otp")
+    global otp
+    global result
+    global current_session
+    email = request.form.get("email")
+    print("Email to send OTP:", email)
+    admin = request.form.get("adminemail")
+    user = request.form.get("user")
+    data["email"] = email
+    data["admin"] = admin
+    data["user"] = user
+    import generate_otp
+    result = generate_otp.generate_opt(email)
+    #current_session["OTP"] = result
+    print("OTP Result:", result)
+    return render_template("forgot_password.html", otp=result, data=data)
 
 
 @app.route("/validate_otp", methods=["POST", "GET"])
 def validate_otp():
-  otp = request.form.get("otp")
-  if otp == current_session["OTP"]:
-    return "OTP Matched"
-  else:
-    return "OTP Does not Match"
+  if request.method == "POST":
+    user = data["user"]
+    email = data["email"]
+    admin = data["admin"]
+    OTP = request.form.get("OTP")
+    print("OTP from previous result:", result)
+    if OTP == result:
+      print("OTP Matched")
+      return render_template("password_reset.html")
+    else:
+      print("OTP Does not match")
+      flash("OTP Does not match!!", "error")
+      return "OTP Does not match!!", "error"
 
 
 app.run(host='0.0.0.0', port=81, debug=True)
