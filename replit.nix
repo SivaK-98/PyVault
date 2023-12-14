e89cd756d2dd586d5894bcce3b3e006eb42fce4e
SivaK-98 <hastthakanni@outlook.com>
2023-12-14 16:31:57 +0000

    Changed the view of multiple pages

--- replit.nix
+++ replit.nix
@@ -1,5 +1,17 @@
 { pkgs }: {
   deps = [
+    pkgs.rustc
+    pkgs.pkg-config
+    pkgs.openssl
+    pkgs.libxcrypt
+    pkgs.libiconv
+    pkgs.cargo
     pkgs.mailutils
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.rustc
+      pkgs.libxcrypt
+    ];
+  };
 }
